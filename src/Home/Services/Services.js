913f47ea0f2752154d9600e982dc56c2d3a5090a
rana135 <rana286090@gmail.com>
2022-04-18 21:43:11 +0600

    finally done

--- src/Home/Services/Services.js
+++ src/Home/Services/Services.js
@@ -5,7 +5,7 @@ import './Services.css';
 const Services = () => {
     const [services, setServices] = useState([])
     useEffect(()=>{
-        fetch("services.json")
+        fetch("Services.json")
         .then(res => res.json())
         .then(data => setServices(data))
     },[])
